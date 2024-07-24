8da8b0fa7d528ee742287b5fa77457d6db95bd94
azartechnet <azartechnet@gmail.com>
2024-07-24 10:57:45 +0530

    updated

--- WebApp/WebApp2/reactjs/rajaapp/src/index.js
+++ WebApp/WebApp2/reactjs/rajaapp/src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/*function Sample()
+{
+   return(
+    <div>
+      <label>UserName</label>
+      <input type="text" placeholder="Enter your name"/>
+      <br/>
+      <label>Password</label>
+      <input type="password" placeholder="Enter your password"/>
+      <br/>
+      <button>Submit</button>
+    </div>
+   )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const r1 = ReactDOM.createRoot(document.getElementById("root"));
+r1.render(<Sample/>);*/
+
+//Basic Rendering
+
+/*const Sample=()=>{
+  return(
+    <div>
+      <h1>My First React App</h1>
+      <p>React is a JavaScript library for building user interfaces.</p>
+    
+    </div>
+    )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'));
+r1.render(<Sample/>);*/
+
+//Dynamic Rendering
+
+const App=()=>{
+  const username="mohamed";
+  const password="123456";
+
+  return(
+
+    <div>
+      <h1>My First React App {username} and {password}</h1>
+      <p>{new Date().toLocaleDateString()}</p>
+      <p>React is a JavaScript library for building user interfaces.</p>
+      <label>UserName</label>
+      <input type="text" placeholder="Enter your name"/>
+      <br/>
+      <label>Password</label>
+      <input type="password" placeholder="Enter your password"/>
+      <br/>
+      <button>Submit</button>
+    </div>
+
+  )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'));
+r1.render(<App/>);
