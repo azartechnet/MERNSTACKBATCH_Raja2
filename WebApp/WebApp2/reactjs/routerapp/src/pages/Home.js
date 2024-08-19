22480112795a579e49f0480046ca5b04808fd5cd
azartechnet <azartechnet@gmail.com>
2024-08-19 10:57:42 +0530

    updated

--- WebApp/WebApp2/reactjs/routerapp/src/pages/Home.js
+++ WebApp/WebApp2/reactjs/routerapp/src/pages/Home.js
@@ -1,4 +1,34 @@
-const Home=()=>{
-    return<h1>This is Home Page</h1>
+import  c1  from './images/c1.jpeg';
+import  c2  from './images/c2.jpeg';
+import  c3 from './images/c3.jpeg';
+
+const Home=()=>
+{
+    return(
+        <>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={c1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={c2} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={c3} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+        </>
+    )
 }
 export default Home;
--- WebApp/WebApp2/reactjs/routerapp/src/pages/Layout.js
+++ WebApp/WebApp2/reactjs/routerapp/src/pages/Layout.js
@@ -1,20 +1,32 @@
-import { Outlet,Link } from "react-router-dom";
 
-const Layout=()=>{
-    return( 
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/Contact">Contact</Link></li>
-                    <li><Link to="/Blogs">Blogs</Link></li>
-                    <li><Link to="/NoPage">NoPage</Link></li>
 
-                </ul>
-            </nav>
-            <Outlet/>
-        </div>
-    )
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">My Website</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/Blogs">Blogs</Nav.Link>
+              <Nav.Link as={Link} to="/NoPage">NoPage</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <Outlet />
+      </Container>
+    </div>
+  );
 }
 
 export default Layout;
